acc96249529d32426541aa3a4614f67fc4d1b700
Whitny Edwards <wne4e2@gmail.com>
2019-08-25 17:26:57 -0400

    update workouts controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -11,6 +11,18 @@ class WorkoutsController < ApplicationController
   end
 
   def create
+    @workout = @current_user.
+    created_workouts.build(
+      workout_params)
+      if @workout.save
+        redirect_to workout_path(@workout)
+      else
+        render :new
+      end
   end
 
+private
+  def workout_params
+    params.require[:workout].permit(:name, :distance, :pace, :category)
+  end
 end
