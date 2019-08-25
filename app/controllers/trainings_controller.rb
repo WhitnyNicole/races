8ac77a0e4655f40fa664c771e62c87e0447d7ada
Whitny Edwards <wne4e2@gmail.com>
2019-08-25 19:41:23 -0400

    update trainings controller create action

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -2,8 +2,24 @@ class TrainingsController < ApplicationController
 
 def new
   @training = Training.new
+  @training.build_workout
+end
+
+def create
+  @training = Training,new(training_params)
+  if @training.save
+    redirect_to training_path(@training)
+  else
+    render :new
+  end
 end
 
 
+private
 
+  def training_params
+    params.require(:training).permit(:date, :feeling,
+      :race_id, :workout_id, :workout_attributes:[:name,
+        :category, :pace, :distance])
+  end
 end
