eaea78bdca7964ebbdcb8a47602ef34f5663d151
Whitny Edwards <wne4e2@gmail.com>
2019-08-25 16:19:22 -0400

    add has_secure_password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :completed_workouts, foreign_key: "user_id", class_name: "Workout"
   has_many :races
   has_many :completed_workouts, through: :races, source: :workouts
+  has_secure_password
+  
 end
