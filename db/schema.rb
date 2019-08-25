75e04724065be437b6e9b3883be15b69709bd56b
Whitny Edwards <wne4e2@gmail.com>
2019-08-25 16:11:03 -0400

    add user_id to races

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_25_200341) do
+ActiveRecord::Schema.define(version: 2019_08_25_200736) do
 
   create_table "races", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_08_25_200341) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "sessions", force: :cascade do |t|
